6d06ba9db7f2f40358574baf1c70c1b419452e11
tulio1057 <tuliojfb283@gmail.com>
2025-11-06 19:37:46 -0300

    Estilização da Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,12 @@
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
-import logo from "../assets/Logo.png";
 import Button from "./Button";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo-container">
-        <img src={logo} alt="Logo da Cafeteria" className="logo-img" />
-        <h1 className="logo-text">Miauteria</h1>
+        <h1 className="logo-text">Fontes Aguas Novas</h1>
       </div>
 
       <ul className="nav-links">
